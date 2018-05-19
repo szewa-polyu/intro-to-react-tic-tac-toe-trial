a00666c58b8917b5574622cced0d35dcee2c3f68
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-05-19 21:20:05 +0800

    Completed!

--- src/index.js
+++ src/index.js
@@ -3,26 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
+  const squareValue = props.value;
+  let btnClassName = "square";
+  if (squareValue === xSymbol()) {
+    btnClassName += " x-square";
+  } else if (squareValue === oSymbol()) {
+    btnClassName += " o-square";  
+  }
+  if (props.isOneOfWinnerThree) {
+    btnClassName += " winner-square";
+  }
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className={btnClassName} onClick={props.onClick}>
+      {squareValue}
     </button>
   )
 }
 
-class Board extends React.Component {  
-  renderSquare(i) {    
+class Board extends React.Component {
+  constructor(props) {
+    super(props);    
+  }
+
+  renderSquare(i, isOneOfWinnerThree) {    
     return ( 
       <Square 
         value={this.props.squares[i]}
+        isOneOfWinnerThree={isOneOfWinnerThree}
         onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const numOfRows = 3;
-    const numOfCols = 3;
+    const numOfRows = this.props.rows;
+    const numOfCols = this.props.cols;
     let rowIndices = [];
     let colIndices = [];
     for (let i = 0; i < numOfRows; i++) {
@@ -36,9 +51,14 @@ class Board extends React.Component {
       return (
         <div key={row} className="board-row">
           {colIndices.map(col => {
+            const squareIdx = convertRowAndColumnToSquareIndex(row, col, numOfCols);
+            let isOneOfWinnerThree = false;
+            if (this.props.winnerThree !== null) {
+              isOneOfWinnerThree = this.props.winnerThree.includes(squareIdx);
+            }
             return (
               <span key={col}>
-                {this.renderSquare(row * numOfCols + col)}
+                {this.renderSquare(squareIdx, isOneOfWinnerThree)}
               </span>
             );
           })}
@@ -74,12 +94,13 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{
-        squares: Array(9).fill(null),
+        squares: Array(props.rows * props.cols).fill(null),
         lastMovePos: -1,
         lastMoveTurn: null
       }],
       stepNumber: 0,
-      xIsNext: true,
+      xIsNext: props.isXStartFirst,
+      isSortMoveDescend: props.isDefaultSortMoveDescend,
     }
   }
 
@@ -90,7 +111,7 @@ class Game extends React.Component {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    squares[i] = this.state.xIsNext ? xSymbol() : oSymbol();
     this.setState({
       history: history.concat([{
         squares: squares,
@@ -109,52 +130,69 @@ class Game extends React.Component {
     });
   }
 
-  convertSquareIndexToRowAndColumn(idx) {
-    return {
-      row: Math.floor(idx / 3),
-      col: idx % 3
-    };
+  reverseMoves() {
+    this.setState((prevState, props) => ({
+      isSortMoveDescend: !prevState.isSortMoveDescend
+    }));
   }
 
   render() {
+    const numOfRows = this.props.rows;
+    const numOfCols = this.props.cols;
+
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winnerThree = calculateWinner(current.squares);
 
-    const moves = history.map((step, move) => {
-      const lastMoveRowAndColumn = this.convertSquareIndexToRowAndColumn(step.lastMovePos);
+    let moves = history.map((step, move) => {
+      const lastMoveRowAndColumn = convertSquareIndexToRowAndColumn(step.lastMovePos, numOfCols);
       const desc = move ?
         'Go to move #' + move + ' (' + step.lastMoveTurn + ': ' + lastMoveRowAndColumn.row + ', ' + lastMoveRowAndColumn.col + ')' :
         'Go to game start (player: row, col)';
       const isMoveCurrentlySelected = this.state.stepNumber === move;
+      const btnClassName = isMoveCurrentlySelected ? 'selected-move' : '';
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)} 
-            className={isMoveCurrentlySelected ? 'selected-move' : ''}>
+            className={btnClassName}>
             {desc}
           </button>
         </li>
       );
     });
 
+    if (this.state.isSortMoveDescend) {
+      moves = moves.reverse();
+    }
+
     let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
+    if (winnerThree) {
+      status = 'Winner: ' + current.squares[winnerThree[0]];
+    } else if (moves.length === numOfRows * numOfCols + 1) {
+      // draw game
+      status = 'Draw game';
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = 'Next player: ' + (this.state.xIsNext ? xSymbol() : oSymbol());
     } 
 
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
+          <Board
+            rows={numOfRows}
+            cols={numOfCols}
             squares={current.squares}
+            winnerThree={winnerThree}
             onClick={(i) => this.handleClick(i)}
           />
-
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <div>
+            <button onClick={() => this.reverseMoves()}>
+              Reverse moves
+            </button>
+          </div>
           <ol>{moves}</ol>
         </div>
       </div>
@@ -165,7 +203,9 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Game rows={3} cols={3} 
+    isXStartFirst={false} 
+    isDefaultSortMoveDescend={true} />,
   document.getElementById('root')
 );
 
@@ -183,8 +223,27 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return lines[i];
     }
   }
   return null;
+}
+
+function convertSquareIndexToRowAndColumn(idx, numOfCols) {
+  return {
+    row: Math.floor(idx / numOfCols),
+    col: idx % numOfCols
+  };
+}
+
+function convertRowAndColumnToSquareIndex(row, col, numOfCols) {
+  return row * numOfCols + col;
+}
+
+function xSymbol() {
+  return 'X';
+}
+
+function oSymbol() {
+  return 'O';
 }
